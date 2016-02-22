77cb6f7b2a6a3d53189a0a281bd6674ef42b5058
Kalle13 <kkiiskinen1@gmail.com>
2016-02-21 21:35:25 -0700

    Update vi_curve_tracer.h
    Added SPI structure and global variable

--- vi_curve_tracer.h
+++ vi_curve_tracer.h
@@ -7,7 +7,18 @@
 #ifndef VI_CURVE_TRACER_H
 #define VI_CURVE_TRACER_H
 
-extern int g_4_bit_operation;
+extern int g_4_bit_operation; // global variable for LCD 4-bit operation
 
+// SPI structure (needed for global variable g_spi_map)
+typedef struct S_SPI_PIO{
+						int clock_phase;
+						int clock_polarity;
+						int word_size;
+						int begin_delay;
+						int end_delay;
+						int data_delay;	
+						} T_SPI_PIO;
+						
+extern T_SPI_PIO g_spi_map[]; // global array of type T_SPI_PIO
 
 #endif
